dc78fd34094aa5f6a46bb149f4193a7456976268
DutchSlav <wouterrovers@gmail.com>
2022-12-08 18:48:11 +0100

    v1.0.0-BETA Changes:  - Added Wallet: Manages the player's money.
    By DutchSlav

--- src/main/java/io/vaclst/adventure/Adventure.java
+++ src/main/java/io/vaclst/adventure/Adventure.java
@@ -2,6 +2,6 @@ package io.vaclst.adventure;
 
 public class Adventure {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, world!");
     }
 }
